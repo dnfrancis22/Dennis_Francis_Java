34317974ee0883e75f890939a6ab69fa1881acd3
Dennis Francis <dnfrancis22@gmail.com>
2021-06-06 18:03:55 -0400

    created test for convertMonth in ConverterIf then wrote code for convertMonth to pass the test.

--- M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/main/java/com/company/ConverterIf.java
+++ M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/main/java/com/company/ConverterIf.java
@@ -5,7 +5,11 @@ import com.company.interfaces.Converter;
 public class ConverterIf implements Converter {
     @Override
     public String convertMonth(int monthNumber) {
-        return null;
+        String month = "";
+        if (monthNumber == 1) {
+            month = "January";
+        }
+        return month;
     }
 
     @Override
--- M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/test/java/com/company/ConverterIfTest.java
+++ M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/test/java/com/company/ConverterIfTest.java
@@ -1,13 +1,21 @@
 package com.company;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ConverterIfTest {
+    ConverterIf converterIf;
+
+    @Before
+    public void setUp() {
+        converterIf = new ConverterIf();
+    }
 
     @Test
     public void convertMonth() {
+        assertEquals("January", converterIf.convertMonth(1));
     }
 
     @Test
