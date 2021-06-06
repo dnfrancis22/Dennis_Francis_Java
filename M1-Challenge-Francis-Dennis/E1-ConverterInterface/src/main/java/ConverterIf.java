ddbd8e4aa75ca2144cac008f7f160d5486aa3df9
Dennis Francis <dnfrancis22@gmail.com>
2021-06-06 17:08:52 -0400

    moved converter interface into com.company package. created ConverterIf and implemented the methods from Converter

---
+++ M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/main/java/ConverterIf.java
@@ -0,0 +1,13 @@
+import com.company.Converter;
+
+public class ConverterIf implements Converter {
+    @Override
+    public String convertMonth(int monthNumber) {
+        return null;
+    }
+
+    @Override
+    public String convertDay(int dayNumber) {
+        return null;
+    }
+}
