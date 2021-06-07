391b7fab294ab6aa02859834f54904fa29e633cd
Dennis Francis <dnfrancis22@gmail.com>
2021-06-06 22:35:05 -0400

    Had to Modify tests

--- M1-Challenge-Francis-Dennis/E2-CalculatorObject/src/test/java/CalculatorTest.java
+++ M1-Challenge-Francis-Dennis/E2-CalculatorObject/src/test/java/CalculatorTest.java
@@ -1,8 +1,10 @@
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
+
     Calculator calc;
 
     @Before
@@ -10,35 +12,40 @@ public class CalculatorTest {
         calc = new Calculator();
     }
 
-    @org.junit.Test
-    public void add() {
+    @Test
+    public void shouldAddTwoIntegers() {
+        assertEquals(3, calc.add(1, 2));
     }
 
-    @org.junit.Test
-    public void testAdd() {
+    @Test
+    public void shouldAddTwoDoubles() {
+        assertEquals(5.3, calc.add(2.2d, 3.1d), .0001);
+    }
+    @Test
+    public void shouldSubtractTwoIntegers() {
+        assertEquals(3, calc.subtract(5, 2));
     }
 
-    @org.junit.Test
-    public void subtract() {
+    @Test
+    public void shouldAddSubtractDoubles() {
+        assertEquals(2.1, calc.subtract(5.2d, 3.1d), .0001);
+    }
+    @Test
+    public void shouldMultiplyTwoIntegers() {
+        assertEquals(2, calc.multiply(1, 2));
     }
 
-    @org.junit.Test
-    public void testSubtract() {
+    @Test
+    public void shouldMultiplyTwoDoubles() {
+        assertEquals(6.82, calc.multiply(2.2d, 3.1d), .0001);
+    }
+    @Test
+    public void shouldDivideTwoIntegers() {
+        assertEquals(2, calc.divide(4, 2));
     }
 
-    @org.junit.Test
-    public void multiply() {
-    }
-
-    @org.junit.Test
-    public void testMultiply() {
-    }
-
-    @org.junit.Test
-    public void divide() {
-    }
-
-    @org.junit.Test
-    public void testDivide() {
+    @Test
+    public void shouldDivideTwoDoubles() {
+        assertEquals(1.6, calc.divide(4d, 2.5d), .0001);
     }
 }
