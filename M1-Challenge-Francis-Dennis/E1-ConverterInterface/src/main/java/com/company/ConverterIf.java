5d3df228cd951b6df27202272d8ecc26f1727a84
Dennis Francis <dnfrancis22@gmail.com>
2021-06-06 20:08:37 -0400

    finished if else statements and re ran tests

--- M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/main/java/com/company/ConverterIf.java
+++ M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/main/java/com/company/ConverterIf.java
@@ -5,23 +5,56 @@ import com.company.interfaces.Converter;
 public class ConverterIf implements Converter {
     @Override
     public String convertMonth(int monthNumber) {
-        String month = "";
-        if (monthNumber < 1 || monthNumber >12) {
-            month = "Please enter a number between 1 and 12";
-        } else if (monthNumber == 1) {
+        String month;
+        if (monthNumber == 1) {
             month = "January";
-        }
+        } else if (monthNumber == 2) {
+            month = "February";
+        } else if (monthNumber == 3) {
+            month = "March";
+        } else if (monthNumber == 4) {
+            month = "April";
+        } else if (monthNumber == 5) {
+            month = "May";
+        } else if (monthNumber == 6) {
+            month = "June";
+        } else if (monthNumber == 7) {
+            month = "July";
+        } else if (monthNumber == 8) {
+            month = "August";
+        } else if (monthNumber == 9) {
+            month = "September";
+        } else if (monthNumber == 10) {
+            month = "October";
+        } else if (monthNumber == 11) {
+            month = "November";
+        } else if (monthNumber == 12) {
+            month = "December";
+        } else
+            month = "Please enter a number between 1 and 12";
+
         return month;
     }
 
     @Override
     public String convertDay(int dayNumber) {
-        String day = "";
-        if (dayNumber < 1 || dayNumber > 7) {
-            day = "Please enter a number between 1 and 7";
-        } else if (dayNumber == 1) {
+        String day;
+        if (dayNumber == 1) {
             day = "Sunday";
-        }
+        } else if (dayNumber == 2) {
+            day = "Monday";
+        } else if (dayNumber == 3) {
+            day = "Tuesday";
+        } else if (dayNumber == 4) {
+            day = "Wednesday";
+        } else if (dayNumber == 5) {
+            day = "Thursday";
+        } else if (dayNumber == 6) {
+            day = "Friday";
+        } else if (dayNumber == 7) {
+            day = "Saturday";
+        } else
+            day = "Please enter a number between 1 and 7";
         return day;
     }
 }
