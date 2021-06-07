ae6c02ca45edee423d53945f0c64b93854a282ba
Dennis Francis <dnfrancis22@gmail.com>
2021-06-06 20:30:11 -0400

    wrote switch cases for convertDay and ConvertMonth and passed tests

--- M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/main/java/com/company/ConverterSwitch.java
+++ M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/main/java/com/company/ConverterSwitch.java
@@ -5,11 +5,34 @@ import com.company.interfaces.Converter;
 public class ConverterSwitch implements Converter {
     @Override
     public String convertMonth(int monthNumber) {
-        return null;
+        return switch (monthNumber) {
+            case 1 -> "January";
+            case 2 -> "February";
+            case 3 -> "March";
+            case 4 -> "April";
+            case 5 -> "May";
+            case 6 -> "June";
+            case 7 -> "July";
+            case 8 -> "August";
+            case 9 -> "September";
+            case 10 -> "October";
+            case 11 -> "November";
+            case 12 -> "December";
+            default -> "Please enter a number between 1 and 12";
+        };
     }
 
     @Override
     public String convertDay(int dayNumber) {
-        return null;
+        return switch (dayNumber) {
+            case 1 -> "Sunday";
+            case 2 -> "Monday";
+            case 3 -> "Tuesday";
+            case 4 -> "Wednesday";
+            case 5 -> "Thursday";
+            case 6 -> "Friday";
+            case 7 -> "Saturday";
+            default -> "Please enter a number between 1 and 7";
+        };
     }
 }
