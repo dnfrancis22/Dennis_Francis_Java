28eefbd28d48f6433a3e2a95cb2234d8cb281882
Dennis Francis <dnfrancis22@gmail.com>
2021-06-06 20:17:47 -0400

    created tests  for ConverterSwitch

--- M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/test/java/com/company/ConverterSwitchTest.java
+++ M1-Challenge-Francis-Dennis/E1-ConverterInterface/src/test/java/com/company/ConverterSwitchTest.java
@@ -1,16 +1,26 @@
 package com.company;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ConverterSwitchTest {
+    ConverterSwitch converterSwitch;
+
+    @Before
+    public void setUp() {converterSwitch = new ConverterSwitch();}
 
     @Test
     public void convertMonth() {
+        assertEquals("January", converterSwitch.convertMonth(1));
+        assertEquals("December", converterSwitch.convertMonth(12));
+        assertEquals("Please enter a number between 1 and 12", converterSwitch.convertMonth(14));
     }
 
     @Test
     public void convertDay() {
+        assertEquals("Sunday", converterSwitch.convertDay(1));
+        assertEquals("Please enter a number between 1 and 7", converterSwitch.convertDay(14));
     }
 }
