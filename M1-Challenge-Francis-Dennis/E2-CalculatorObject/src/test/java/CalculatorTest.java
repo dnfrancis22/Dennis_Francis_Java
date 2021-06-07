aa1a7c05816dc01a535da45d17b6d33daf0312a1
Dennis Francis <dnfrancis22@gmail.com>
2021-06-06 21:27:00 -0400

    Created Calculator Object then created CalculatorTest then wrote code to pass tests

---
+++ M1-Challenge-Francis-Dennis/E2-CalculatorObject/src/test/java/CalculatorTest.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    Calculator calc;
+
+    @Before
+    public void setUp() {
+        calc = new Calculator();
+    }
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void testAdd() {
+    }
+
+    @org.junit.Test
+    public void subtract() {
+    }
+
+    @org.junit.Test
+    public void testSubtract() {
+    }
+
+    @org.junit.Test
+    public void multiply() {
+    }
+
+    @org.junit.Test
+    public void testMultiply() {
+    }
+
+    @org.junit.Test
+    public void divide() {
+    }
+
+    @org.junit.Test
+    public void testDivide() {
+    }
+}
